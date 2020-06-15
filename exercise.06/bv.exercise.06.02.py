d0868651cbdd52747dae14830ba07da7685761a9
patrickz98 <patrickz98@users.noreply.github.com>
2020-06-15 13:28:12 +0200

    Check in.

--- exercise.06/bv.exercise.06.02.py
+++ exercise.06/bv.exercise.06.02.py
@@ -9,7 +9,7 @@ import skimage.io
 # pro Durchlauf, um über die Pixel zu iterieren, und nutzt zur Berechnung keine Funktionen wie
 # np.mean() oder np.var(). Testet eure Funktion an dem bekannten Testbild mandrill.png aus
 # dem Moodle.
-def formel_1(bild: numpy.array):
+def formel_1(bild: numpy.array) -> (float, float):
     cols = bild.shape[0]
     rows = bild.shape[1]
 
@@ -29,16 +29,14 @@ def formel_1(bild: numpy.array):
 
     standard_deviation /= cols * rows
 
-    # print("Formel 1")
-    # print("mean", mean)
-    # print("standard_deviation", standard_deviation)
+    return standard_deviation, mean
 
 
 # 2. Implementiert nun eine Funktion für die Varianzberechnung in einem Durchlauf nach Formel 2.
 # Nutzt dazu erneut unbedingt zwei verschachtelte for-Schleifen pro Durchlauf, um über die Pixel
 # zu iterieren, und nutzt zur Berechnung erneut keine Funktionen wie np.mean() oder np.var().
 # Testet eure Funktion wieder am Testbild mandrill.png.
-def formel_2(bild: numpy.array):
+def formel_2(bild: numpy.array) -> (float, float):
     cols = bild.shape[0]
     rows = bild.shape[1]
 
@@ -53,9 +51,7 @@ def formel_2(bild: numpy.array):
     mean /= cols * rows
     standard_deviation = (standard_deviation / (cols * rows)) - (mean ** 2)
 
-    # print("Formel 2")
-    # print("mean", mean)
-    # print("standard_deviation", standard_deviation)
+    return standard_deviation, mean
 
 
 def time_test(bild: numpy.array):
@@ -77,5 +73,12 @@ def time_test(bild: numpy.array):
 bild = skimage.io.imread("mandrill.png")
 time_test(bild)
 
-# formel_1(bild)
-# formel_2(bild)
+# standard_deviation, mean = formel_1(bild)
+# print("Formel 1")
+# print("mean", mean)
+# print("standard_deviation", standard_deviation)
+
+# standard_deviation, mean = formel_2(bild)
+# print("Formel 2")
+# print("mean", mean)
+# print("standard_deviation", standard_deviation)
